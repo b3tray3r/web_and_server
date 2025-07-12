b8e231b4b261dd58b1a04b9c436eeee3485657d0
b3tray3r <40644354+b3tray3r@users.noreply.github.com>
2025-07-12 15:24:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ function handleSearch(users) {
 // Отправка нового пользователя на сервер
 async function addUser(data) {
   try {
-    const res = await fetch(`${apiUrl}/users`, {
+    const res = await fetch(apiUrl), {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
